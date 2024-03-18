5200504780bf6d6fa7feca4f7417087f116b6e6d
lord-is-me <luoj041106@gmail.com>
2024-03-18 21:05:09 +0800

    v-status 0.01

--- js/checkServer.js
+++ js/checkServer.js
@@ -1,6 +1,6 @@
 const mask = document.querySelector(`.mask`)
 function checkServerAvailability() {
-    axios.get('https://10.242.98.255:3000/checkServerAvailability')
+    axios.get('https://120.236.163.67:3000/checkServerAvailability')
         .then((response) => {
             mask.style.display = `none`;
         })
